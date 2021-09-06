74f2feddb99f18ceec9517fe89e8b8f8b99f7162
thainole <thainole@gmail.com>
2021-09-06 13:13:40 -0500

    jugando

---
+++ src/api.js
@@ -0,0 +1,48 @@
+
+function arabicToRoman(number){
+  let roman;
+  const romanNumList = {M:1000,CM:900, D:500,CD:400, C:100, XC:90,L:50, XV: 40, X:10, IX:9, V:5, IV:4, I:1};
+  let a;
+
+  if(typeof(number) !== 'string' || typeof(number) !== 'number') 
+    return 'Please enter a valid arabic number';
+
+  if(number > 3999)
+    return 'Enter a number between 1 and 3999';
+    
+  
+  for(let key in romanNumList) {
+    a = Math.floor(number / romanNumList[key]);
+    if (a >= 0) {
+        for(let i = 0; i < a; i++){
+          roman += key;
+        }
+      }
+    number = number % romanNumList[key];
+  }
+  
+  return roman;
+}
+
+
+function romanToArabic(romanNumber) {
+
+  if(typeof(romanNumber) !== 'string') 
+    return 'Please enter a valid roman number';
+
+  romanNumber = romanNumber.toUpperCase();
+  const romanNumList = ["CM","M","CD","D","XC","C","XL","L","IX","X","IV","V","I"];
+  const corresp = [900,1000,400,500,90,100,40,50,9,10,4,5,1];
+  let index =  0, num = 0;
+  for(let rn in romanNumList){
+    index = romanNumber.indexOf(romanNumList[rn]);
+    while(index != -1){
+      num += parseInt(corresp[rn]);
+      romanNumber = romanNumber.replace(romanNumList[rn],"-");
+      index = romanNumber.indexOf(romanNumList[rn]);
+    }
+  }
+  return num;
+}
+
+console.log(romanToArabic('XX'))
